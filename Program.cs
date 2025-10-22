171f5e4b35bc634951b83ee00bd6ee2ac195c1fa
pacofustier <pacofustier@etik.com>
2025-10-22 06:15:09 +0200

    RemoveOpenApi

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -14,7 +13,6 @@ var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
